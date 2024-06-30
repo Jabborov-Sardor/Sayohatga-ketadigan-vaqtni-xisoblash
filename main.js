c36c3275862e885a72be4c0344caeba3238088eb
Jabborov-Sardor <jabborovsardor0097.net@gmail.com>
2024-06-30 22:16:00 +0500

    End

--- main.js
+++ main.js
@@ -6,12 +6,30 @@ document.getElementById("btn").onclick = function () {
   const plane = 800;
   function allTime(road, spead) {
     let results = road / spead
-    return results
+    let hours = Math.floor ( results)
+    let minut = Math.round ((results- hours) * 60 )
+    // let secund = Math.round((results - minut) * 60);
+    return {hours,minut}
   }
-  // console.log();
  let manResult = allTime(distance,man) 
-  let bikeRusult = allTime(distance,bike)
+  let bikeResult = allTime(distance,bike)
+  let carResult = allTime(distance,car)
+  let planeResult = allTime(distance,plane)
+ if( distance > 0 ) {
+  document.querySelector("#manResult").innerHTML = `${manResult.hours} soat ${manResult.minut} minut`;
+  document.querySelector("#bikeResult").innerHTML = `${bikeResult.hours} soat ${bikeResult.minut} minut`;
+  document.querySelector("#carResult").innerHTML = `${carResult.hours} soat ${carResult.minut} minut`;
+  document.querySelector("#planeResult").innerHTML = `${planeResult.hours} soat ${planeResult.minut} minut`;
 
-  document.querySelector("#manResult").innerHTML = `${manResult}`;
-  document.querySelector("#bikeResult").innerHTML = `${bikeRusult}`;
+ }else if (distance === '' ) {
+  document.querySelector("#manResult").innerHTML = 'Masofani kiriting';
+  document.querySelector("#bikeResult").innerHTML = 'Masofani kiriting';
+  document.querySelector("#carResult").innerHTML = 'Masofani kiriting';
+  document.querySelector("#planeResult").innerHTML = 'Masofani kiriting';
+ } else{
+  document.querySelector("#manResult").innerHTML = 'Notugri masofa kiritdingiz';
+  document.querySelector("#bikeResult").innerHTML = 'Notugri masofa kiritdingiz';
+  document.querySelector("#carResult").innerHTML = 'Notugri masofa kiritdingiz';
+  document.querySelector("#planeResult").innerHTML = 'Notugri masofa kiritdingiz';
+ }
 };
